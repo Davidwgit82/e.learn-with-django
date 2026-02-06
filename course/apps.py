26f968940945c88ab464e622d4dc05a4f88a610e
Davidwgit82 <davidyvwork082@gmail.com>
2026-02-06 07:21:30 +0000

    create a user admin, modified css and settings.py

--- course/apps.py
+++ course/apps.py
@@ -1,5 +1,21 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
 class CourseConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'course'
+
+    '''def ready(self):
+        from django.contrib.auth import get_user_model
+        import os
+
+        User = get_user_model()
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'leasi-ad')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'leasi@example.com')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'E.web.py12')
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username, email, password)
+            print(f"Superuser {username} créé avec succès !")
+'''
--- web/settings.py
+++ web/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
